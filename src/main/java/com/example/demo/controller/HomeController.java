d38fbc006ddb219763ba0355aef294435d16ffa9
vandan1501 <88451852+vandan1501@users.noreply.github.com>
2023-04-13 12:20:13 +0530

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @RequestMapping("/")
     public @ResponseBody String greeting() {
-        return "Hello, I am vandan kathiriya";
+        return "Hello, world";
     }
 
 }
